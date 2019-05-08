1583e6a1ca06f50316bd8b5042aca0879ff8e1f8
Kevin Pelzel <kevinpelzel22@gmail.com>
2019-05-07 22:59:26 -0600

    adding second two to test2

--- pi/test2.py
+++ pi/test2.py
@@ -8,6 +8,11 @@ oled = OLED()
 # other is a small square over to the right
 window_one = OLEDWindow(oled,0,0,256,64)
 
-for i in range(255):
+for i in range(256):
     window_one.set_pixel(i,0,i)
+window_one.draw_screen_buffer()
+
+for i in range(256):
+    if i % 15 == 0:
+        window_one.set_pixel(i,1,1)
 window_one.draw_screen_buffer()
